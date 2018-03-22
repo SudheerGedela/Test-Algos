ff7c622ed4b0b2a8f610d83835a99939172cd94c
vthalapu <vamsi.thalapulapalli@optum.com>
2018-03-21 23:20:05 -0500

    Added linked list

---
+++ src/main/java/recursions/TestRecursions.java
@@ -0,0 +1,33 @@
+package recursions;
+
+/**
+ * Project: algo-test
+ * Package: recursions
+ * <p>
+ * User: vthalapu
+ * Date: 3/21/18
+ * Time: 11:04 PM
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestRecursions {
+
+    static public void main(String... args) {
+        TestRecursions testRecursions = new TestRecursions();
+        System.out.println(testRecursions.factRecursion(10));
+
+    }
+
+    int factRecursion(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return n * factRecursion(n - 1);
+
+    }
+
+
+}
