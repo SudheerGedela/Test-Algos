878ce87f4c40f839e4b3f834f85e8da17d59dec5
vthalapu <vamsi.thalapulapalli@optum.com>
2018-04-09 23:12:27 -0500

    Adding text

--- src/main/java/generics/TestGenerics.java
+++ src/main/java/generics/TestGenerics.java
@@ -12,4 +12,38 @@ package generics;
  * To change this template use File | Settings | File Templates.
  */
 public class TestGenerics {
+
+    public static void main(String... args) {
+        System.out.println(new TestGenerics().testingGenerics(10));
+        System.out.println(new TestGenerics().testingGenerics("Hello Vamsi"));
+        System.out.println(new TestGenerics().testingGenerics('C'));
+        System.out.println(new TestGenerics().testingGenerics(new String("New Object")));
+
+    }
+
+    //wrong
+//    public void testGen1(<T> t){
+//
+//    }
+
+    public <T> T testGen() {
+        return null;
+    }
+
+    public <T> T testGen2(T t) {
+        return null;
+    }
+
+    public <T> T testingGenerics(T t) {
+        if (t instanceof Integer)
+            System.out.println("T is a Integer -> " + t);
+        if (t instanceof String)
+            System.out.println("T is a String -> " + t);
+        if (t instanceof Character)
+            System.out.println("T is a Character -> " + t);
+
+        return t;
+    }
+
+
 }
