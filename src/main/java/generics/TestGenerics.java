85d20ca8f073e1c981d8967e57c10a2e3459c9c6
vthalapu <vamsi.thalapulapalli@optum.com>
2018-04-09 23:05:08 -0500

    Adding text

---
+++ src/main/java/generics/TestGenerics.java
@@ -0,0 +1,15 @@
+package generics;
+
+/**
+ * Project: Test-Algos
+ * Package: generics
+ * <p>
+ * User: vthalapu
+ * Date: 4/9/18
+ * Time: 11:04 PM
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestGenerics {
+}
